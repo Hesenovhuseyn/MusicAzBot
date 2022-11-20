dc30bc0850a189b034eb6742fbc075fdc2a9bef7
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-11-20 18:52:01 +0400

    update add_user.py

--- helpers/database/add_user.py
+++ helpers/database/add_user.py
@@ -2,7 +2,7 @@
 # Sahib @HuseynH
 # Repo Açığdısa İcazəsis Götürmə Oğlum
 
-from AylinRobot.config import Config
+from MusicAzBot.config import Config
 from helper.database.access_db import db
 from pyrogram import Client
 from pyrogram.types import Message
