955bc6c0a257e7e82ca2d1fdc5acfca077ac57c1
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-11-20 19:59:45 +0400

    update broadcast.py

--- helpers/broadcast.py
+++ helpers/broadcast.py
@@ -11,7 +11,7 @@ import datetime
 import aiofiles.os
 import traceback
 from MusicAzBot.config import Config
-from helper.database.access_db import db
+from helpers.database.access_db import db
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
