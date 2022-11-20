5db86b0cfb931d963cfcf78457f5720c35d9a5a4
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-11-20 22:18:18 +0400

    update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,5 +1,22 @@
 __version__ = "0.7.0"
 
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+def start() -> scoped_session:
+    engine = create_engine(Config.MONGODB_CLI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
+
+
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from MusicAzBot.config import Config
 
