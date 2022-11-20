1be08f2127b4bb82fe3ebf1e703fde0feab1ebd8
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-11-20 18:36:05 +0400

    update qarsılama.py

--- MusicAzBot/Plugin/qarsılama.py
+++ MusicAzBot/Plugin/qarsılama.py
@@ -2,10 +2,10 @@ import random
 from time import time
 from random import choice
 from pyrogram.types import Message
-from AylinRobot import AylinRobot as app
-from AylinRobot import LOGGER
+from MusicAzBot import MusicAzBot as app
+from MusicAzBot import LOGGER
 from pyrogram import idle, filters
-from AylinRobot.config import Config
+from MusicAzBot.config import Config
 from pyrogram import Client, filters
 
 xs = ["Salam,, və Qrupuna Xoş gəlmisiniz.",
