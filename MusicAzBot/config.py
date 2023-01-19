75dbd7ef17ee587f1222f25dde8483b629f7d833
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-19 16:06:20 +0400

    update config.py

--- MusicAzBot/config.py
+++ MusicAzBot/config.py
@@ -5,15 +5,15 @@ class Config:
 
    API_ID = int(os.getenv("API_ID", "8953338"))
    API_HASH = os.getenv("API_HASH", "fe21f223cb02d8f7c1cbda651f553a45")
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "5976700768:AAGKIP1hMx4w_lOJ6ptNVevEySsYDvk_Bbo")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "DunyaMusicBot")
-   OWNER_ID = int(os.environ.get("OWNER_ID","5840833148"))
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "2142897671:AAEHf0HdxZN3vm3eUcTz72dB38ma8vY_AaE")
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "MusicAzBot")
+   OWNER_ID = int(os.environ.get("OWNER_ID","5865605067"))
    OWNER_NAME = os.environ.get("OWNER_NAME", "Husidi") 
    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://music:music@cluster0.sh6h4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001817585114"))
-   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "huzurum98")
-   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001710291377"))
+   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001898638971"))
+   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "MusicAzPlaylist")
+   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001692410500"))
    COMMAND_PREFIXES = list(os.environ.get("COMMAND_PREFIXES", "/ ! .").split())
    
    
