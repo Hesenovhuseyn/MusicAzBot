98a140df98caa931f60f7afd32877f36830c65f3
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2024-03-26 01:35:42 +0400

    Delete helpers directory

--- helpers/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = "0.7.0"
-
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from MusicAzBot.config import Config
-
-
-MONGODB_CLI = MongoClient(Config.MONGODB_URI)
-db = MONGODB_CLI.wbb
--- helpers/database/access_db.py
+++
@@ -1,8 +0,0 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
-
-from MusicAzBot.config import Config
-from helpers.database.database import Database
-
-db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
