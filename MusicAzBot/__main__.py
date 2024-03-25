435188fff7e2ac3026d6a811ff1e28a46b0ff89f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2024-03-26 01:45:43 +0400

    update __main__.py

--- MusicAzBot/__main__.py
+++ MusicAzBot/__main__.py
@@ -38,27 +38,6 @@ async def start(client, message):
         reply_markup=Translation.START_BUTTONS
     )
     
-    
-@app.on_message(filters.private & filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply)
-async def _broadcast(_, client: Message):
-    await broadcast_handler(client)
-
-
-@app.on_message(filters.private & filters.command("status") & filters.user(Config.OWNER_ID))
-async def show_status_count(_, client: Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
-    await client.reply_text(
-        text=f"**Ümumi Disk Sahəsi:** {total} \n**İstifadə edilmiş Sahə:** {used}({disk_usage}%) \n**Boş Yer:** {free} \n**CPU  İstifadə:** {cpu_usage}% \n**RAM İstifadəsi:** {ram_usage}%\n\n**{Config.BOT_USERNAME}-Ümumi İstifadəçiləri:** `{total_users}`"  ,
-        parse_mode="Markdown",
-        quote=True
-    )
 
 app.start()
 LOGGER.info(f"{Config.BOT_USERNAME} Uğurla Başladı Sahibim {Config.OWNER_NAME}")
